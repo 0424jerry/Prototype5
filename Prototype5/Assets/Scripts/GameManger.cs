6722953be44728a8248512e515e3106e9c1a73b1
0424jerry <jerrygithub@gms.ndhu.edu.tw>
2021-01-03 00:59:01 +0800

    5-1

---
+++ Prototype5/Assets/Scripts/GameManger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManger : MonoBehaviour
+{
+    public List<GameObject> targets;
+
+    public float spawnRate = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpwanTarget());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpwanTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0,targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+}
