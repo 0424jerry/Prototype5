47ed2dab832afe842383eb8fab76a8f074885fb4
0424jerry <jerrygithub@gms.ndhu.edu.tw>
2021-01-03 16:48:59 +0800

    5done

--- Prototype5/Assets/Scripts/Target.cs
+++ Prototype5/Assets/Scripts/Target.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
-
+using UnityEngine.SceneManagement;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
@@ -12,7 +12,10 @@ public class Target : MonoBehaviour
     private float xRange = 4; 
     private float maxT = 10;
     private float ySpawnPos = -6;
-    
+    private GameManager gameManager;
+    public int pointV;
+
+    public ParticleSystem explosionParticle;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +23,7 @@ public class Target : MonoBehaviour
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(),RandomTorque(),RandomTorque(),ForceMode.Impulse);
         transform.position = RandomSpawnPos();
+        gameManager = GameObject.Find("Game Manger").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -45,11 +49,25 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown()
     {
-        Destroy(gameObject);
+        if(gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle,transform.position,explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointV);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
+    }
+    
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
